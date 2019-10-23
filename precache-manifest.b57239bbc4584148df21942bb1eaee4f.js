c0388d4571f8d50b5f30761558509c363ecff572
Pensor <patrick.feldtmann@gmail.com>
2019-10-23 17:27:41 +0200

    Updates

---
+++ precache-manifest.b57239bbc4584148df21942bb1eaee4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b625a42bada90e30db7c1817ee1d7de",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "b06f77a0372eee356541",
+    "url": "/ravenous/static/css/main.aa818709.chunk.css"
+  },
+  {
+    "revision": "10e495104ae6cae53425",
+    "url": "/ravenous/static/js/2.5f31ffe2.chunk.js"
+  },
+  {
+    "revision": "b06f77a0372eee356541",
+    "url": "/ravenous/static/js/main.9628b10f.chunk.js"
+  },
+  {
+    "revision": "1bddd6d240b7ef8b9785",
+    "url": "/ravenous/static/js/runtime-main.5c846237.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
